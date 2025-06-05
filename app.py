f59524c440a332dca1d9c7bbdeb11e9dbc2c88bc
Ayush Soni <141045200+AyushSoni14@users.noreply.github.com>
2025-06-06 02:19:16 +0530

    Update app.py line 39

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forms.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forms.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/form.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
